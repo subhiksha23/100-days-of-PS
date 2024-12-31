c4aecec571b3b90823c98e6de08aa2d98984aab9
subhiksha23 <161231594+subhiksha23@users.noreply.github.com>
2024-12-31 21:24:35 +0530

    final commit

---
+++ prime.c
@@ -0,0 +1,20 @@
+int main() {
+    int n = 29;
+    int cnt = 0;
+    if (n <= 1)
+        printf("%d is NOT prime\n", n);
+    else {
+     for (int i = 1; i <= n; i++) {
+               if (n % i == 0)
+                cnt++;
+        }
+     if (cnt > 2)
+            printf("%d is NOT prime\n", n);
+
+        // else it is prime
+        else
+            printf("%d is prime", n);
+    }
+
+    return 0;
+}
