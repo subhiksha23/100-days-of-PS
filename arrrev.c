4df0411c11483c1ac86e324bb1a9cd0d8de4bb9e
subhiksha23 <161231594+subhiksha23@users.noreply.github.com>
2025-02-26 23:11:28 +0530

    final commit

---
+++ arrrev.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n,index;
+scanf("%d",&n);
+int arr[n];
+for(index=0;index<n;index++)
+{
+scanf("%d",&arr[n]);
+}
+for(index=n-1;index>=0;index--)
+{
+printf("%d",&arr[n]);
+}
+return 0;
+}
