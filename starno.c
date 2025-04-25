775606d6b9770ca8bfd2f6f469bc74a26a2a2c0d
subhiksha23 <161231594+subhiksha23@users.noreply.github.com>
2025-04-25 20:02:30 +0530

    fin

---
+++ starno.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+int n1,n2;
+scanf("%d%d",&n1,&n2);
+for(int i=q1; n1<=n2; i++)
+{
+for(int j=n2;j<=n2;j++)
+  {
+  printf("%d",j)
+    }
+printf("\n");
+n2--;
+}
+}
